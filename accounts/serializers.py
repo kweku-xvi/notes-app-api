2e1ca1de94d6035d8d3504655a51b61257ed6385
kweku-xvi <nksarps@gmail.com>
2023-11-06 20:34:06 +0000

    modified register login view

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import UserModel
+from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserModel
+        model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 
@@ -12,18 +12,37 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=50, min_length=6, write_only=True)
 
     class Meta:
-        model = UserModel
+        model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({'email':'Email is already in use'})
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({'username':'Username is already in use'})
+        return attrs
+    
+
     def create(self, validated_data):
-        return UserModel.objects.create_user(**validated_data)
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class LoginUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=50, min_length=6, write_only=True)
 
     class Meta:
-        model = UserModel
+        model = User
         fields = ['id','first_name', 'last_name','email', 'username', 'password', 'token']
 
         read_only_fields = ['token']
