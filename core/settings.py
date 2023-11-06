8908dd72f8f0d143666cf2aa0a9e5f1346be58aa
kweku-xvi <nksarps@gmail.com>
2023-11-06 20:32:29 +0000

    modifed custom base user name from UserModel to User

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-AUTH_USER_MODEL = 'accounts.UserModel'
+AUTH_USER_MODEL = 'accounts.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
