b20db9b057a7ade9ff771052d5f4ce71d771da95
kweku-xvi <nksarps@gmail.com>
2023-11-07 20:24:49 +0000

    added endpoints for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', views.register_user, name='register-user'),
     path('login/', views.user_login, name='user-login'),
     path('users/', views.get_all_users_view, name='users'),
+    path('password-reset/', views.password_reset_view, name='reset_password'),
+    path('password-reset-confirm/', views.password_reset_confirm_view, name='password_reset_confirm_view')
 ]
